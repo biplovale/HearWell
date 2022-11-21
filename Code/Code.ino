671b9f479ad9fddf14d42d888f7c9b273ed7ee56
Biplov Ale <biplovale2020@gmail.com>
2022-11-20 20:38:05 -0500

    NoPotInitialTester

--- Code/Code.ino
+++ Code/Code.ino
@@ -1,44 +1,40 @@
 #include "Volume.h"
 #include <math.h>
 #include <ArxContainer.h>
-#define window_size 10
 
 //Initial setup 
-int window[window_size] = {0};
-//int volProfile[8] = {125, 250, 500, 1000, 2000, 3000, 4000, 8000};
-arx::vector<int> interestedFreq {};
+int freqArrSize = 8;
+arx::array<int,8> interestedFreq {125, 250, 500, 1000, 2000, 3000, 4000, 8000};
 arx::map<int, int> profile {};
-int noOfOctaves = 6;
-int volumePot = A0;
-int frequencyPot = A1;
-int buttonPin = 7;
-Volume vol;
+
+int buttonPin = 2;
 int buttonNew;
-//int freqIndex = 0;
-int curVol;
+
+Volume tonePlayer;
 int curFreq;
+
+int curVol = 0;//ranges 0 to 255
+int curFreqIndex = 0;
 int stopValue = 0;
 
 //Helper function definations
-void push(int val);
-int take_avg();
-int scaleToOctaves(int octaves);
-int playFrequency(int frequency);           //plays the frequency with a volume control and returns the vol
+int getCurrentFrequency(int freqIndex);
+void playFrequency(int frequency, int volume);           //plays the frequency with a volume control and returns the vol
 
 void setup() {
   Serial.begin(9600);
-  vol.begin(); //default pin = D5; alternate = D6
+  tonePlayer.begin(); //default pin = D5; alternate = D6
   pinMode(buttonPin, INPUT);
 }
 
 void loop() {
   buttonNew = digitalRead(buttonPin);
 
-  while (buttonNew == 1 && stopValue <= noOfOctaves){ //stops the hearing test if the profiling is done 
-    curFreq = scaleToOctaves(noOfOctaves);
-    curVol = playFrequency(curFreq);
+  while (buttonNew == 1 && stopValue < freqArrSize){ //stops the hearing test if the profiling is done 
+    curFreq = getCurrentFrequency(curFreqIndex);
+    playFrequency(curFreq, curVol);
     buttonNew = digitalRead(buttonPin);
-    Serial.println(buttonNew);
+    Serial.println(curVol);
     //executes when button is pressed
     if (buttonNew == 0 ){
       Serial.println("Button Pressed ");
@@ -46,72 +42,38 @@ void loop() {
       Serial.print(curFreq);
       Serial.print(" vol: ");
       Serial.print(curVol);
-      interestedFreq.push_back(curFreq);
       profile[curFreq] = curVol;
+      curFreqIndex += 1;
+      curVol = 0;
       stopValue += 1;
-    }
-  }
+      Serial.println(stopValue);
 
-  Serial.println(stopValue);
-  //prints the profile
-  if (stopValue >= noOfOctaves){
-    vol.noTone();
-    Serial.println();
-    for (int i = 0; i <= noOfOctaves; i++){
-      Serial.print(interestedFreq[i]);
-      Serial.print(": ");
-      Serial.print(profile[interestedFreq[i]]);
-      Serial.println();
+      delay(2000);
+    }
+    delay(3000);
+    
+    if (curVol >= 0 && curVol < 255){
+      curVol = curVol + 5;
     }
   }
- 
   
-  delay(20000);
-}
-
-//Helper function implementation
-void push(int val){
-  int i = 0;
-  for(i = 0; i < window_size; i++){
-    window[i-1] = window[i];
+  //prints the profile
+  if (stopValue >= freqArrSize){
+    tonePlayer.noTone();
+    Serial.println();
+    for (int i = 0; i < freqArrSize; i++){
+      Serial.print(interestedFreq[i]); Serial.print(": "); Serial.println(profile[interestedFreq[i]]);
+    }
   }
-  window[window_size - 1] = val; 
 }
 
-int take_avg(){
-  long sum = 0;
-  int i = 0;
-  for(i = 0; i < window_size; i++){
-    sum += window[i];
-  }
-  return sum/window_size;
-}
-
-int scaleToOctaves(int octaves){
-  //potentiometer stabalized inputs
-  int cur_sample = 0;
-  cur_sample = (250.0 * pow(2.0, (octaves *analogRead(frequencyPot)) / 1023.0));
-  Serial.print(cur_sample);
-  Serial.print(" avg freq: ");
-  push(cur_sample);
-  int result = take_avg();
-  Serial.println(result);
+int getCurrentFrequency(int freqIndex){
+  int result = interestedFreq[freqIndex];
 
   return result;
 }
 
-int playFrequency(int frequency){
-  //potentiometer stabalized inputs
-  int cur_sample = 0;
-  cur_sample = (255.0 / 1023.0) * analogRead(volumePot);
-  Serial.print(cur_sample);
-  Serial.print(" avg: ");
-  push(cur_sample);
-  int result = take_avg();
-  Serial.println(result);
-
+void playFrequency(int freq, int vol){
   //generates an audio frequency
-  vol.tone(frequency, result);
-
-  return result;
+  tonePlayer.tone(freq, vol);
 }
