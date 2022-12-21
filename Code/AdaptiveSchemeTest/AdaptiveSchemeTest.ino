f50eaa9096417b62c9324df521e0432e0d1f8c34
Biplov Ale <biplovale2020@gmail.com>
2022-12-21 13:29:28 -0500

    working test script

--- Code/AdaptiveSchemeTest/AdaptiveSchemeTest.ino
+++ Code/AdaptiveSchemeTest/AdaptiveSchemeTest.ino
@@ -6,38 +6,40 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   profile[125.0] = 100;
-  profile[250.0] = 50;
-//  profile[500] = 100;
+  profile[250.0] = 100;
+  profile[500.0] = 100;
 //  profile[1000] = 100;
 //  profile[2000] = 100;
 //  profile[3000] = 100;
 //  profile[4000] = 100;
 //  profile[8000] = 100;
 
-  Serial.println();
-  for (const auto& m : profile)
-  {
-      Serial.print("{");
-      Serial.print(m.first); Serial.print(",");
-      Serial.print(m.second);
-      Serial.println("}");
-  }
-
-  recursiveSimpson(125.0, 250.0, 0.1, NULL);
-
-  Serial.println();
-  for (const auto& m : profile)
-  {
-      Serial.print("{");
-      Serial.print(m.first); Serial.print(",");
-      Serial.print(m.second);
-      Serial.println("}");
-  }
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+Serial.println();
+  for (const auto& m : profile)
+  {
+      Serial.print("{");
+      Serial.print(m.first); Serial.print(",");
+      Serial.print(m.second);
+      Serial.println("}");
+  }
+  
+  recursiveSimpson(125.0, 250.0, 0.000001, simpleSimpson(125.0, 250.0));
 
+  Serial.println();
+  for (const auto& m : profile)
+  {
+      Serial.print("{");
+      Serial.print(m.first); Serial.print(",");
+      Serial.print(m.second);
+      Serial.println("}");
+  }
+
+  while(Serial.available() == 0){}
 }
 
 float simpleSimpson(float a, float b){
@@ -61,7 +63,7 @@ float simpleSimpson(float a, float b){
   return (interval/6.0) * (profile[a] + (4 * meanVolPoint) + profile[b]);
 }
 
-void recursiveSimpson(float xF, float xL, float eps, float intLast){
+float recursiveSimpson(float xF, float xL, float eps, float intLast){
   float xMean = (xF+xL)/2.0;
   Serial.println(xMean);
   float intL;
@@ -81,16 +83,17 @@ void recursiveSimpson(float xF, float xL, float eps, float intLast){
   delay(1000);
   Serial.println(abs(intTotal - intLast) < 15 * eps);
   if(abs(intTotal - intLast) < 15 * eps){
-//    return ((16*intTotal) - intLast) / 15.0;
-      return;
+    return ((16*intTotal) - intLast) / 15.0;
   }
   else{
-//    return recursiveSimpson(xF, xMean, eps/2, intL) + recursiveSimpson(xMean, xL, eps/2, intR);
-    Serial.println("leftRecursion Start");
-    recursiveSimpson(xF, xMean, eps/2, intL);
-    Serial.println("leftRecursion End");
-    Serial.println("rightRecursion Start");
-    recursiveSimpson(xMean, xL, eps/2, intR);
-    Serial.println("leftRecursion End");
+     return recursiveSimpson(xF, xMean, eps/2, intL) + recursiveSimpson(xMean, xL, eps/2, intR);
+//    Serial.println("leftRecursion Start");
+//    float recurLeft = recursiveSimpson(xF, xMean, eps/2, intL);
+//    Serial.println("leftRecursion End");
+//    Serial.println("rightRecursion Start");
+//    float recurRight = recursiveSimpson(xMean, xL, eps/2, intR);
+//    Serial.println("leftRecursion End");
+//
+//    return recurLeft + recurRight;
   }
 } 
