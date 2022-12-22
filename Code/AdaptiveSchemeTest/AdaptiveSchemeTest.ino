f8206dbe6077f1a4820b5490986c0afc8c5721c2
Yoseph Dexyos <71545773+redsprites@users.noreply.github.com>
2022-12-22 13:40:53 -0500

    resolved conflicts

--- Code/AdaptiveSchemeTest/AdaptiveSchemeTest.ino
+++ Code/AdaptiveSchemeTest/AdaptiveSchemeTest.ino
@@ -7,15 +7,9 @@ int recStep = 0;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-<<<<<<< HEAD
   profile[0] = (sin(0) * sin(0));
   profile[2*pi] = (sin(2* pi) * sin(2*pi));
 //  profile[500.0] = 100;
-=======
-  profile[125.0] = 100;
-  profile[250.0] = 100;
-  profile[500.0] = 100;
->>>>>>> f50eaa9096417b62c9324df521e0432e0d1f8c34
 //  profile[1000] = 100;
 //  profile[2000] = 100;
 //  profile[3000] = 100;
@@ -36,14 +30,9 @@ Serial.println();
       Serial.println("}");
   }
   
-<<<<<<< HEAD
   recursiveSimpson(0, 2*pi, 0.000001, simpleSimpson(0, 2*pi));
 
 delay(2000);
-=======
-  recursiveSimpson(125.0, 250.0, 0.000001, simpleSimpson(125.0, 250.0));
-
->>>>>>> f50eaa9096417b62c9324df521e0432e0d1f8c34
   Serial.println();
   for (const auto& m : profile)
   {
@@ -96,7 +85,6 @@ float recursiveSimpson(float xF, float xL, float eps, float intLast){
   delay(1000);
   Serial.println(abs(intTotal - intLast) < 15 * eps);
   if(abs(intTotal - intLast) < 15 * eps){
-<<<<<<< HEAD
     recStep += 1;
     return ((16*intTotal) - intLast) / 15.0;
   }
@@ -108,12 +96,7 @@ float recursiveSimpson(float xF, float xL, float eps, float intLast){
     else{
       return intLast;
     }
-=======
-    return ((16*intTotal) - intLast) / 15.0;
-  }
-  else{
-     return recursiveSimpson(xF, xMean, eps/2, intL) + recursiveSimpson(xMean, xL, eps/2, intR);
->>>>>>> f50eaa9096417b62c9324df521e0432e0d1f8c34
+
 //    Serial.println("leftRecursion Start");
 //    float recurLeft = recursiveSimpson(xF, xMean, eps/2, intL);
 //    Serial.println("leftRecursion End");
